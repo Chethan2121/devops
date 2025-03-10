8bcb9542c0a345ccd472e13a2feac1b0a9cf80d2
Chethan AR <134262730+Chethan2121@users.noreply.github.com>
2025-03-10 10:10:58 +0530

    Update ed.java

--- ed.java
+++ ed.java
@@ -1,27 +1,9 @@
 public class ed {
 
   public static void main(String[] args) {
-
-    int num = 29;
-    boolean flag = false;
-
-    // 0 and 1 are not prime numbers
-    if (num == 0 || num == 1) {
-        flag = true;
-    }
-
-    for (int i = 2; i <= num / 2; ++i) {
-
-      // condition for nonprime number
-      if (num % i == 0) {
-        flag = true;
-        break;
-      }
-    }
-
-    if (!flag)
-      System.out.println(num + " is a prime number.");
-    else
-      System.out.println(num + " is not a prime number.");
+    int a = 10;
+    int b = 20;
+    int c = a+b;
+    System.out.println("Sum = " + c);
   }
 }
